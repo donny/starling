7ed31fcc47e22fadc6fce6abe4f3f696ee8abc9a
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-05-08 18:10:58 +1000

    Add files from Mix

---
+++ lib/starling.ex
@@ -0,0 +1,18 @@
+defmodule Starling do
+  @moduledoc """
+  Documentation for Starling.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Starling.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/starling_test.exs
@@ -0,0 +1,8 @@
+defmodule StarlingTest do
+  use ExUnit.Case
+  doctest Starling
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
